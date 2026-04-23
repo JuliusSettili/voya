f3bac474e9ad996e826cc30d30f5c937a6694297
JuliusAlexanderSettili <julius.settili@outlook.de>
2026-04-23 15:49:31 +0200

    fix: set basename and base path to '/voya/' in router and Vite configuration

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  basename: "/voya/",
   ssr: false,
   future: {
     v8_middleware: true,
