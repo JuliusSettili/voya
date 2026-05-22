2734d9df84c3b60545a80b644214f9dee8ff5349
JuliusAlexanderSettili <julius.settili@outlook.de>
2026-05-22 12:22:11 +0200

    refactor: remove unused AuthProfile type from auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -1,10 +1,5 @@
 import { getSupabaseClient } from "./supabaseClient";
 
-export type AuthProfile = {
-  id: string;
-  display_name: string;
-};
-
 export async function getUser() {
   const supabase = getSupabaseClient();
   const {
