501f0f0e779964dcaaabe2148369215f6887cc96
Mau099 <timo.feller@students.hs-mainz.de>
2026-06-05 20:28:34 +0200

    added profile page and EditField for Async functions

---
+++ app/components/AsyncEditField.tsx
@@ -0,0 +1,64 @@
+// this component is a text field with a edit button. if the button is cliecked, the text field becomes editable. if the button is clicked again, the text field becomes read only again and the onChange function is called with the new value.
+
+import { useState } from "react";
+import { MdEdit, MdCheck } from "react-icons/md";
+
+export default function AsyncEditField({
+  value,
+  onChange,
+  className,
+}: {
+  value: string;
+  onChange: (newValue: string) => Promise<void>;
+  className?: string;
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputValue, setInputValue] = useState(value);
+  const [error, setError] = useState("");
+  const [isSaving, setIsSaving] = useState(false);
+
+  const handleEditClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event?.preventDefault();
+    if (!isEditing) {
+      setError("");
+      setIsEditing(true);
+      return;
+    }
+    try {
+      setIsSaving(true);
+      setError("");
+      await onChange(inputValue);
+      setIsEditing(false);
+    } catch (error) {
+      setError(error instanceof Error ? error.message : "Speichern der Änderungen ist fehlgeschlagen.");
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
+  return (
+    <div className={`flex items-center gap-2 ${className || ""}`}>
+      {!isEditing ? (
+        <span>{inputValue}</span>
+      ): (
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          readOnly={!isEditing}
+          className="input"
+      />)}
+
+      <button
+        type="button"
+        onClick={handleEditClick}
+        className="btn btn-square btn-ghost btn-sm"
+        title={isEditing ? "Änderungen speichern" : "Bearbeiten"}
+      >
+        {isEditing ? <MdCheck size={16} /> : <MdEdit size={16} />}
+      </button>
+
+      {error && <div className="text-sm text-error">{error}</div>}
+    </div>
+  );
+}
