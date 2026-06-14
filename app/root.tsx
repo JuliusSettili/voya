548f27694cfd8418da8ef189c82b298faab0046a
JuliusAlexanderSettili <julius.settili@outlook.de>
2026-06-14 11:50:50 +0200

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,11 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    size: "32x32",
+    href: "/voya/favicon.png",
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
